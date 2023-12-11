c33830144e6273b659e2e5f9f8447f0aaeee676d
Sam Luedtke <luedtkesj@gmail.com>
2023-12-11 01:54:40 -0600

    Basic movement in sample scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,25 +10,83 @@ public class Player : FPSObject
 	[SerializeField] Weapon[] weapons;
 	int currentWeaponIndex = 0; //index in the arraylist
 
+	[Header("Movement")]
+	[SerializeField] private float movementSpeed = 5f;
+	[SerializeField] private float gravity = 2f;
+	[SerializeField] private float jumpForce = 5f;
+
+	private CharacterController characterController = null;
+	private Camera camera = null;
+	private Vector3 currentMovement = Vector3.zero;
+	private float rotationX = 0;
+
+	[Header("Look")]
+	[SerializeField, Range(0.1f, 10)] private float lookSpeedX = 2;
+    [SerializeField, Range(0.1f, 10)] private float lookSpeedY = 2;
+	[SerializeField, Range(1, 100)] private float upperLookLimit = 80;
+    [SerializeField, Range(1, 100)] private float lowerLookLimit = 80;
+
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+		camera = GetComponentInChildren<Camera>();
+		rotationX = camera.transform.localRotation.y;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+	public void OnJump(InputAction.CallbackContext context)
+	{
+		if (!characterController.isGrounded) {
+			Debug.Log("Not grounded!");
+			return;
+		}
+
+		if (context.phase == InputActionPhase.Performed) {
+			currentMovement.y = jumpForce;
+			
+		}
+	}
+
+	public void OnMove(InputAction.CallbackContext context) {
+		var input = context.ReadValue<Vector3>();
+		currentMovement = (transform.TransformDirection(Vector3.forward) * input.z) + (transform.TransformDirection(Vector3.right) * input.x);
+
+
+	}
+
+	public void OnLookRight(InputAction.CallbackContext context) {
+		float rotation = context.ReadValue<float>();
+		transform.rotation *= Quaternion.Euler(0, rotation, 0);
+
+	}
+
+	public void OnLookUp(InputAction.CallbackContext context) {
+		if (context.phase == InputActionPhase.Performed) {
+			rotationX -= context.ReadValue<float>() * lookSpeedY;
+			rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+			camera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+		}
+	}
+
     // Update is called once per frame
     void Update()
     {
-        
+		if (!characterController.isGrounded)
+			currentMovement.y -= gravity * Time.deltaTime;
+
+		characterController.Move(currentMovement * movementSpeed * Time.deltaTime);
     }
 
-	public void OnJump(InputAction.CallbackContext context)
-	{
-		Debug.Log("I'm Jumping");
-	}
 
-	//current weapon player has out 
-	public Weapon GetWeapon() {
+
+
+    //current weapon player has out 
+    public Weapon GetWeapon() {
 		return weapons[currentWeaponIndex];
 	}
 
