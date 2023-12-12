3d0cc4307180e8aa7e3de48ae13acbd62d02744e
Sam Luedtke <luedtkesj@gmail.com>
2023-12-12 01:14:24 -0600

    Added crouching ability

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,29 +9,45 @@ public class Player : FPSObject
 
 
 	[SerializeField] Weapon[] weapons;
-	int currentWeaponIndex = 0; //index in the arraylist
 
 	[Header("Movement")]
-	[SerializeField] private float movementSpeed = 5f;
-	[SerializeField] private float gravity = 2f;
+	[SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 10f;
+    [SerializeField] private float gravity = 2f;
 	[SerializeField] private float jumpForce = 5f;
+	[SerializeField] private float crouchSpeed = 3f;
 
-	private CharacterController characterController = null;
-	private Camera camera = null;
-	private Vector3 currentMovement = Vector3.zero;
-	private float rotationX = 0;
-
-	[Header("Look")]
+    [Header("Look")]
 	[SerializeField, Range(0.1f, 10)] private float lookSpeedX = 2;
     [SerializeField, Range(0.1f, 10)] private float lookSpeedY = 2;
 	[SerializeField, Range(1, 100)] private float upperLookLimit = 80;
     [SerializeField, Range(1, 100)] private float lowerLookLimit = 80;
 
 
+	[Header("Crouch")]
+	[SerializeField] private float crouchHeight = 0.5f;
+	[SerializeField] private float standingHeight = 2f;
+	[SerializeField] private float timeToCrouch = 0.25f;
+	[SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
+	[SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
+
+	private bool isCrouching = false;
+	private bool duringCrouchAnimation = false;
+
+
+    private CharacterController characterController = null;
+    private Camera camera = null;
+    private Vector3 currentMovement = Vector3.zero;
+    private float rotationX = 0;
+    private bool isSprinting = false;
+    private Vector3 inputDir;
+    int currentWeaponIndex = 0; //index in the arraylist
+
     private void Awake() {
         characterController = GetComponent<CharacterController>();
 		camera = GetComponentInChildren<Camera>();
 		rotationX = camera.transform.localRotation.y;
+		standingCenter = characterController.center;
     }
 
     // Start is called before the first frame update
@@ -41,17 +58,19 @@ public class Player : FPSObject
 
 	public void OnJump(InputAction.CallbackContext context)
 	{
-		if (!characterController.isGrounded) {
+		if (!characterController.isGrounded || duringCrouchAnimation) {
 			return;
 		}
 
 		if (context.phase == InputActionPhase.Performed) {
+			if (isCrouching) {
+				ToggleCrouch();
+				return;
+			}
 			currentMovement.y = jumpForce;
 			
 		}
 	}
-
-	private Vector3 inputDir;
 	public void OnMove(InputAction.CallbackContext context) {
 		 inputDir = context.ReadValue<Vector3>();
     }
@@ -70,13 +89,34 @@ public class Player : FPSObject
 		}
 	}
 
+	public void OnSprint(InputAction.CallbackContext context) {
+		if (context.phase == InputActionPhase.Started) {
+			Debug.Log("Enabling");
+			isSprinting = true;
+		}
+		else if (context.phase == InputActionPhase.Canceled){
+			Debug.Log("Disabling");
+			isSprinting = false;
+		}
+	}
+
+	public void OnCrouch(InputAction.CallbackContext context) {
+		if (context.phase == InputActionPhase.Performed) {
+			if (duringCrouchAnimation)
+				return;
+			ToggleCrouch();
+		}
+	}
+
     // Update is called once per frame
     void Update()
     {
-		CharacterMovement();
+		HandleMovement();
     }
 
-	private void CharacterMovement() {
+	// Take the input direction and apply it to the character controller
+	// Subtract from the y value if currently in a jump to apply gravity
+	private void HandleMovement() {
         var dir = (transform.TransformDirection(Vector3.forward) * inputDir.z) + (transform.TransformDirection(Vector3.right) * inputDir.x);
 		
 		currentMovement.x = dir.x;
@@ -84,10 +124,45 @@ public class Player : FPSObject
         if (!characterController.isGrounded)
             currentMovement.y -= gravity * Time.deltaTime;
 
-
-        characterController.Move(currentMovement * movementSpeed * Time.deltaTime);
+		float speed = isSprinting ? sprintSpeed : walkSpeed;
+		speed = isCrouching ? crouchSpeed : speed;
+        characterController.Move(currentMovement * speed * Time.deltaTime);
     }
 
+	private void ToggleCrouch() {
+        if(isCrouching) {
+            // check for ceiling
+            if (Physics.Raycast(camera.transform.up, Vector3.up, 1f)) {
+				return;
+            }
+
+        }
+
+		isCrouching = !isCrouching;
+		StartCoroutine(CrouchStand());
+    }
+
+
+	private IEnumerator CrouchStand() {
+		
+
+		duringCrouchAnimation = true;
+		float timeElapsed = 0;
+		float targetHeight = isCrouching ? crouchHeight : standingHeight;
+		float currentHeight = characterController.height;
+		Vector3 targetCenter = isCrouching ? crouchingCenter: standingCenter;
+		Vector3 currentCenter = characterController.center; 
+
+		while (timeElapsed < timeToCrouch) {
+			characterController.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed / timeToCrouch);
+			characterController.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed / timeToCrouch);
+			timeElapsed += Time.deltaTime;
+			yield return null;
+		}
+		characterController.height = targetHeight;
+		characterController.center = targetCenter;
+		duringCrouchAnimation = false;
+	}
 
 
 
@@ -139,12 +214,13 @@ public class Player : FPSObject
 
 	// 
 
-	public bool Airborne() {
+
+	public bool Airborne {
+		get => !characterController.isGrounded;
 		//TODO returns true if player is airborne 
 		//TODO use a bool instead of a function instead?
 		//TODO may want a collider for this, positioned to have coyote time maybe 
 		//ex. checks if airborne to check if you can jump, checks if airborne to determine if that rifle can scope 
-		return false;
 	}
 
 	
