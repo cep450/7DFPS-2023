96f68c8e69fcae5c620c83a28168d2b79939be26
Sam Luedtke <luedtkesj@gmail.com>
2023-12-13 01:02:46 -0600

    Added slide ability when crouch is pressed while sprinting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,13 @@ public class Player : FPSObject
 
 	[SerializeField] Weapon[] weapons;
 
+	[Header("Functional Options")]
+	[SerializeField] private bool canSprint = true;
+	[SerializeField] private bool canJump = true;
+	[SerializeField] private bool canCrouch = true;
+	[SerializeField] private bool canHeadbob = true;
+	[SerializeField] private bool willSlideOnSlopes = true;
+
 	[Header("Movement")]
 	[SerializeField] private float walkSpeed = 5f;
     [SerializeField] private float sprintSpeed = 10f;
@@ -17,6 +24,8 @@ public class Player : FPSObject
 	[SerializeField] private float jumpForce = 5f;
 	[SerializeField] private float crouchSpeed = 3f;
 	[SerializeField] private float slopeSpeed = 6f;
+	[SerializeField] private float slideSpeed = 15f;
+	
 
     [Header("Look")]
 	[SerializeField, Range(0.1f, 10)] private float lookSpeedX = 2;
@@ -32,11 +41,34 @@ public class Player : FPSObject
 	[SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
 	[SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
 
+	[Header("Headbob")]
+	[SerializeField] private float walkBobSpeed = 14f;
+	[SerializeField] private float walkBobAmount = 0.05f;
+	[SerializeField] private float sprintBobSpeed = 18f;
+	[SerializeField] private float sprintBobAmount = 0.1f;
+	[SerializeField] private float crouchBobSpeed = 8f;
+	[SerializeField] private float crouchBobAmount = 0.025f;
+
+	[Header("Sliding")]
+	[SerializeField] private float maxSlideTime = 1;
+	[SerializeField] private float slideForce = 1;
+	[SerializeField] private float slideHeight = 0.35f;
+	[SerializeField] private float timeToSlide = 0.12f;
+	[SerializeField] private float slideYScale;
+	[SerializeField] private Vector3 slidingCenter = new Vector3(0, 0.35f, 0);
+
+	private float slideTimer = 0;
+	private float startYScale = 0;
+	private bool isSliding = false;
+
+	private float defaultYPos = 0;
+	private float timer = 0;
+
 	private Vector3 hitPointNormal;
 	private bool IsSliding
 	{
 		get {
-			if (characterController.isGrounded && Physics.Raycast(transform.position, Vector3.down, out RaycastHit slopeHit, 1f)) {
+			if (characterController.isGrounded && Physics.Raycast(transform.position, Vector3.down, out RaycastHit slopeHit, 2f)) {
 				hitPointNormal = slopeHit.normal;
 				return Vector3.Angle(hitPointNormal, Vector3.up) > characterController.slopeLimit;
 			}
@@ -57,22 +89,31 @@ public class Player : FPSObject
     private bool isSprinting = false;
     private Vector3 inputDir;
     int currentWeaponIndex = 0; //index in the arraylist
-
+	private WaitForSeconds slideDelay = null;
     private void Awake() {
         characterController = GetComponent<CharacterController>();
 		camera = GetComponentInChildren<Camera>();
 		rotationX = camera.transform.localRotation.y;
 		standingCenter = characterController.center;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible= false;
+
+        defaultYPos = camera.transform.localPosition.y;
+		startYScale = transform.localScale.y;
+		slideDelay = new WaitForSeconds(maxSlideTime);
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
 	public void OnJump(InputAction.CallbackContext context)
 	{
+		if (!canJump)
+			return;
+
 		if (!characterController.isGrounded || duringCrouchAnimation) {
 			return;
 		}
@@ -87,11 +128,15 @@ public class Player : FPSObject
 		}
 	}
 	public void OnMove(InputAction.CallbackContext context) {
-		 inputDir = context.ReadValue<Vector3>();
-    }
+		// Don't change directions while sliding
+		if (isSliding)
+			return;
+
+		inputDir = context.ReadValue<Vector3>();
+	}
 
 	public void OnLookRight(InputAction.CallbackContext context) {
-		float rotation = context.ReadValue<float>();
+		float rotation = context.ReadValue<float>() * lookSpeedX;
 		transform.rotation *= Quaternion.Euler(0, rotation, 0);
 
 	}
@@ -105,9 +150,16 @@ public class Player : FPSObject
 	}
 
 	public void OnSprint(InputAction.CallbackContext context) {
+		if (!canSprint)
+			return;
+		
+
 		if (context.phase == InputActionPhase.Started) {
-			Debug.Log("Enabling");
-			isSprinting = true;
+            if (isCrouching) {
+                ToggleCrouch();
+            }
+
+            isSprinting = true;
 		}
 		else if (context.phase == InputActionPhase.Canceled){
 			Debug.Log("Disabling");
@@ -116,16 +168,29 @@ public class Player : FPSObject
 	}
 
 	public void OnCrouch(InputAction.CallbackContext context) {
+		if (!canCrouch)
+			return;
+
 		if (context.phase == InputActionPhase.Performed) {
 			if (duringCrouchAnimation)
 				return;
-			ToggleCrouch();
+
+			if (!isSprinting) {
+				ToggleCrouch();
+			}
+			else {
+				SlideCrouch();
+			}
 		}
 	}
 
     // Update is called once per frame
     void Update()
     {
+		if (canHeadbob) {
+			HandleHeadbob();
+		}
+
 		HandleMovement();
     }
 
@@ -141,8 +206,35 @@ public class Player : FPSObject
 
 		float speed = isSprinting ? sprintSpeed : walkSpeed;
 		speed = isCrouching ? crouchSpeed : speed;
+		speed = isSliding ? slideSpeed : speed;
+
+        if (willSlideOnSlopes && IsSliding) {
+            currentMovement += new Vector3(hitPointNormal.x, -hitPointNormal.y, hitPointNormal.z) * slopeSpeed;
+        }
+
         characterController.Move(currentMovement * speed * Time.deltaTime);
     }
+
+	private void HandleHeadbob() {
+		if (isSliding)
+			return;
+
+		
+		if (Mathf.Abs(currentMovement.x) > 0.1f || Mathf.Abs(currentMovement.z) > 0.1f) {
+			float speed = isSprinting ? sprintBobSpeed : walkBobSpeed;
+			float amt = isSprinting ? sprintBobAmount : walkBobAmount;
+			if (isCrouching) {
+				amt = crouchBobAmount;
+				speed = crouchBobSpeed;
+			}
+
+            timer = (timer + (Time.deltaTime * speed)) % float.MaxValue;
+			camera.transform.localPosition = new Vector3(
+				camera.transform.localPosition.x,
+				defaultYPos + Mathf.Sin(timer) * amt,
+				camera.transform.localPosition.z);
+		}
+	}
 
 	private void ToggleCrouch() {
         if(isCrouching) {
@@ -154,11 +246,12 @@ public class Player : FPSObject
         }
 
 		isCrouching = !isCrouching;
-		StartCoroutine(CrouchStand());
+		StartCoroutine(CrouchStand(isCrouching, crouchHeight, standingHeight, timeToCrouch, crouchingCenter, standingCenter));
     }
 
-
-	private IEnumerator CrouchStand() {
+	// Handle crouch and sliding position transitions
+	private IEnumerator CrouchStand(bool isCrouching, float crouchHeight,
+		float standingHeight, float timeToCrouch, Vector3 crouchingCenter, Vector3 standingCenter) {
 		
 
 		duringCrouchAnimation = true;
@@ -179,6 +272,26 @@ public class Player : FPSObject
 		duringCrouchAnimation = false;
 	}
 
+	private void SlideCrouch() {
+		StartCoroutine(SlideThenCrouch());
+	}
+	private IEnumerator SlideThenCrouch() {
+		yield return CrouchStand(true, slideHeight, standingHeight, timeToSlide, slidingCenter, standingCenter);
+		isSliding = true;
+		yield return slideDelay;
+		isCrouching = true;
+		// Transition to crouch
+		yield return CrouchStand(false, slideHeight, crouchHeight, timeToSlide * 1.2f, slidingCenter, crouchingCenter);
+		isSliding = false;
+	}
+
+	//private void StartSlide() {
+	//	isSliding = true;
+	//}
+
+	//private void SlidingMovement() {
+	//	Vector3 inpu
+	//}
 
 
 	// weapon stuff
